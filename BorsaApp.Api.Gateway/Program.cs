179d4cff89fa358872240656103ad0870247d5ac
ozan muğulkoç <mugulkoc@hotmail.com>
2025-09-21 17:50:26 +0300

    DockerFiles

--- BorsaApp.Api.Gateway/Program.cs
+++ BorsaApp.Api.Gateway/Program.cs
@@ -5,8 +5,7 @@ namespace BorsaApp.Api.Gateway
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+             
 
             builder.Services.AddControllers();
 
