ea583adb25e10223c66c50fd9e00bc975101a177
ozan muğulkoç <mugulkoc@hotmail.com>
2025-09-21 19:55:02 +0300

    Deneme

--- BorsaApp.Api.Gateway/Controllers/WeatherForecastController.cs
+++ BorsaApp.Api.Gateway/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace BorsaApp.Api.Gateway.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "OZAN", "OZAN", "OZAN", "OZAN", "OZAN", "OZAN", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
